7d41048a13aa3afaa23695764414ef59010e3df4
NathanTheDev <nathan.smith1922@gmail.com>
2026-03-22 12:17:56 +1100

    chore: removed sidebar

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { Sidebar } from "@/components/ui/Sidebar";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,7 +9,6 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <div className="flex min-h-screen w-full bg-[#FAF0D7]">
-      <Sidebar />
       <main className="flex-1 relative">
         <Outlet />
       </main>
